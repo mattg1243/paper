2bc8df404666f932b9c162cc2978548216258dea
mattg1243 <mattgallucci97@gmail.com>
2021-11-23 14:24:22 -0800

    basic outline of trending list components done diplaying img, ticker, and price in usd for top 7 coins

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Search from './components/Search';
+import TrendingList from './components/TrendingList';
 
 class App extends Component {
   
-  getCoin = async (event) => {
+  state  = {
+    coin: "",
+    trending: [],
+  }
+
+  getCoin = async (event, coinID) => {
     event.preventDefault();
     
-    const coin = event.target.elements.searchName.value;
-    const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coin}`);
-    const data = await response.json();
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.coingecko.com/api/v3/simple/price?ids=${coinID}&vs_currencies=usd`);
+    const price = await response.json();
 
-    console.log(data);
+    this.setState({ coin: price })
+
+  }
+
+  getTrending = async (event) => {
+    event.preventDefault();
+    
+    const response = await fetch(`https://api.coingecko.com/api/v3/search/trending`);
+    const list = await response.json();
+
+    console.log(list['coins'])
+    this.setState({ trending: list['coins'] })
 
   }
   
@@ -23,7 +38,7 @@ class App extends Component {
           <div className="App-header">
             <h1 className="App-title">Paper</h1>
           </div>
-          <Search getCoin={this.getCoin}/>
+          <TrendingList getTrending={this.getTrending} trending={this.state.trending}/>
         </div>
 
       
