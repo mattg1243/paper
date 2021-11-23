1eadda973e502384c9f18461ed6e1708f00f4fc6
mattg1243 <mattgallucci97@gmail.com>
2021-11-23 02:30:17 -0800

    displays current price from simple/price call

---
+++ src/components/Coin.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Coin = (props) => (
+
+    <div>
+        <header><h1>{props.name}</h1></header>
+        <h4>Price: {props.price}</h4>
+        <h4>Volume: {}</h4>
+
+    </div>
+
+)
+
+export default Coin;
