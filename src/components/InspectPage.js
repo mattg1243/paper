bf1292f15b0d551db5f97a05efd081588b476376
mattg1243 <mattgallucci97@gmail.com>
2021-11-29 12:05:20 -0800

    some small changes to the inspect chart

--- src/components/InspectPage.js
+++ src/components/InspectPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { LineChart, Line, Tooltip, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from 'recharts'
+import { AreaChart, Area, Tooltip, ResponsiveContainer, XAxis, YAxis, CartesianGrid } from 'recharts'
 
 const InspectPage = () => {
 
@@ -16,9 +16,10 @@ const InspectPage = () => {
         // const simplePrice = await response.json();
         const fetchedPrice = []
         const chartResponseJSON = await chartResponse.json();
-        for (let i = 0; i < chartResponseJSON['prices'].length; i++) { fetchedPrice[i] = { name: `hr${i}`, price: `${chartResponseJSON['prices'][i][1]}`}  }
+        for (let i = 0; i < chartResponseJSON['prices'].length; i++) { fetchedPrice[i] = { name: `${i}`, price: chartResponseJSON['prices'][i][1]}  }
         min = Math.min(chartData);
         max = Math.max(chartData);
+        console.log(fetchedPrice[1])
         getChartData(fetchedPrice);
     } 
 
@@ -27,22 +28,30 @@ const InspectPage = () => {
     return (
         
         <>
+        <defs>
+            <linearGradient id="chartGradient" x1="0" y1="0" x2="0=" y2="1">
+                <stop offset="0%" stopColor="#8884d8" stopOpacity={0.4} />
+                <stop offset="75%" stopColor="#8884d8" stopOpacity={0.05} />
+            </linearGradient>
+        </defs>
+
         <h1>
             {`${coinID.toUpperCase()}`}
         </h1>
             <div style={{display: 'flex', justifyContent: 'center'}}>
                 <ResponsiveContainer width="80%" height={600}>
-                <LineChart width={500} height={250} data={chartData} margin={{
+                <AreaChart width={500} height={250} data={chartData} margin={{
                     top: 50,
                     right: 30,
                     left: 20,
                     bottom: 5,
                 }} >
                     <Tooltip />
-                    <XAxis />
-                    <YAxis type="number" domain={[ min, max ]}/>
-                    <Line type="natural" dataKey="price" dot={false} stroke="#8884d8" strokeWidth={2}  />
-                </LineChart>
+                    <XAxis axisLine={false} tickLine={true} tickFormatter={() => {return ""}} />
+                    <YAxis axisLine={false} tickLine={true} type="number" domain={[ min, max ]} dataKey="price"/>
+                    <CartesianGrid opacity={.1} vertical={false}/>
+                    <Area type="natural" dataKey="price" dot={false} stroke="#8884d8" strokeWidth={1}  />
+                </AreaChart>
                 </ResponsiveContainer>
                 
             </div>
